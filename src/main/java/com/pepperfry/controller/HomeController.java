eef06e28e814c7048b732b5c6fd22f6e711b82a4
aravindproject <kumar.akumar.aravind@gmail.com>
2018-01-20 19:27:26 +0530

    Initial commit

---
+++ src/main/java/com/pepperfry/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.pepperfry.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	
+	
+	@RequestMapping("/")
+	public ModelAndView index(){
+		return new ModelAndView("index");
+	}
+
+	@RequestMapping("home")
+	public ModelAndView func(){
+		return new ModelAndView("test");
+	}
+}
